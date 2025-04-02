efc0da76ebba709e1b517d7d907970881171be45
dsmcnelly0 <dsmcnelly0@gmail.com>
2025-04-02 21:17:53 +0000

    added output

---
+++ survey/util.py
@@ -0,0 +1,86 @@
+import re, subprocess
+from cryptography.fernet import Fernet
+from django.db import connections
+from .forms import AnswerForm
+
+def getRatingStr(num):
+   FIRST_TUP_VAL = 0
+   SECOND_TUP_VAL = 1
+   str = ""
+   for row in AnswerForm.RATINGS5:
+      if ( row[FIRST_TUP_VAL] == num ):
+         str = row[SECOND_TUP_VAL]
+         break
+   return str
+
+def getConn():
+    return connections['default']
+
+def getQuestionText(question_id):
+    with getConn().cursor() as cur:
+        q = "select question_text from survey_question where id = " + str(question_id)
+        cur.execute(q)
+        dat = cur.fetchall()
+        for item in dat:
+            itemStr = str(item).strip("(,)")
+    try:
+        questionText = itemStr
+    except UnboundLocalError as ulx:
+        return "No record found."
+
+    return questionText
+
+def getQuestions():
+    with getConn().cursor() as cur:
+        q = "select id, question_text from survey_question order by question_text"
+        cur.execute(q)
+        dat = cur.fetchall()
+        # Add hint add head of list.
+        dat.insert(0, (-1, "<Pick question>"))
+    return dat
+
+def getChoices(quest_id):
+    with getConn().cursor() as cur:
+        q = "select id, choice_text from survey_choice where question_id = " + str(quest_id)
+        #q = "select q.question_text, c.id, c.choice_text from survey_choice c join survey_question q on c.question_id = q.id where c.question_id = " + str(quest_id)
+        cur.execute(q)
+        dat = cur.fetchall()
+    return dat
+
+def getResponses():
+    with getConn().cursor() as cur:
+        q = "select r.name, q.question_text, c.choice_text, to_char(r.changedatetime, 'YYYY-fmMM-fmDD HH:MIam') change_time, r.demo_record"
+        q += " from survey_choice c right join survey_question q on c.question_id = q.id left join survey_response r on c.id = r.choice_id"
+        q += " order by r.changedatetime desc, c.changedatetime desc"
+        cur.execute(q)
+        dat = cur.fetchall()
+    return dat
+
+def runFlSsRpt():
+    FIRSTCOL = 0
+    FIFTHCOL = 4
+    head = "<html><body><h2><center>Store</center>"
+    sub = subprocess.run(["./store.sh"], shell=True)
+    print("Check point:", "50.0", "Shell script status:", sub)
+    fl = open("df.out", "r")
+    lin = []
+    rpt = "<br>"
+    #i = 0
+    firstRow = True
+    for ln in fl:
+        if firstRow == False:
+            col = re.split("\s+", ln)
+            ss = col[FIRSTCOL]
+            u = col[FIFTHCOL]
+            rpt += ss + "&emsp;" + u + "<br>"
+            #lin.append(ln)
+            #i += 1
+            print(ln)
+        else:
+            print("Not showing column titles.")
+        firstRow = False
+
+    foot = "</body></html>"
+
+    return head + rpt + foot
+ 
