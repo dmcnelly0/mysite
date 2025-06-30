ad5d8733a2a6b9d810b00a1410f85054834edb81
dmcnelly0 <107365643+dmcnelly0@users.noreply.github.com>
2025-06-30 16:39:47 -0400

    backup before transforming from class to method

--- survey/fil.py
+++ survey/fil.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.views import View
+from django.contrib.auth.decorators import login_required
 
 from .util import runFlSsRpt
 
 class flSs(View):
-    def get(self, req):
-        print("Check point:", "7")
+    @login_required
+    def get(self, req, unm):
+        print("Check point:", "7", unm)
         doc = runFlSsRpt()
 
         return HttpResponse(doc)
