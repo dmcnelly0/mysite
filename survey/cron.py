e27952811d50e965e7fe232ad4c29e4712f37992
dmcnelly0 <107365643+dmcnelly0@users.noreply.github.com>
2025-11-25 15:17:24 -0500

    backup before big change

--- survey/cron.py
+++ survey/cron.py
@@ -1,8 +1,22 @@
-from .models import Pick
-from .util import runFind
+#from /home/ec2-user/mysite.survey.models import Pick
+#from /home/ec2-user/mysite.survey.util import runFind
 
-def cronRun():
-   # Get the latest active record.
-   pick = Pick.objects.filter(active = True).order_by("changedatetime").reverse()
-   print(pick[0])
-   runFind(pick[0].item, pick[0].wsite_cd)
+def run_():
+   ckpt = "0"
+   try:
+      ckpt = "1"
+      from survey.models import Pick
+      from survey.util import runFind
+      # Get the latest active record.
+      ckpt = "2"
+      pick = Pick.objects.filter(active = True).order_by("changedatetime").reverse()
+      print(pick[0])
+      ckpt = "3"
+      runFind(pick[0].item, pick[0].wsite_cd)
+   except Exception as x:
+      fWrite = open("Err.out", "a")
+      fWrite.write("Error at " + ckpt + ": " + str(x) + "\n")
+      fWrite.close()
+
+# if __name__ == "__main__":
+   # run_()
