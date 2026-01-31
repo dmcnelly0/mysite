ac311dd5f38a87bacefd846e9c0dfab40f3a7e16
dsmcnelly0 <dsmcnelly0@gmail.com>
2026-01-31 10:09:25 +0000

    write the date of the run in didrun.out

--- pcron.sh
+++ pcron.sh
@@ -1,3 +1,3 @@
 cd /home/ec2-user/mysite
-echo "Did run." > /home/ec2-user/mysite/didrun.out
+date > /home/ec2-user/mysite/didrun.out
 /home/ec2-user/.pyenv/shims/python3 /home/ec2-user/mysite/cron.py
